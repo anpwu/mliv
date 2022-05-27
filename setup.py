723812e972c41c342d47fda8b287b67a67c2ea77
anpwu <anpwu2019@gmail.com>
2022-05-27 05:14:55 +0000

    Upload - mliv - v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as fh:
 
 setuptools.setup(
     name="mliv",       # 模块名称
-    version="0.0.1",                  # 当前版本
+    version="0.0.2",                  # 当前版本
     author="anpeng wu",               # 作者
     author_email="anpwu2019@gmail.com",        # 作者邮箱
     description="machine learning for instrumental variable (IV) regression",    # 模块简介
@@ -23,6 +23,8 @@ setuptools.setup(
     install_requires=[
         'argparse',
         'pillow',
+        'numba',
+        'cvxopt',
     ],
   # python版本
     python_requires=">=3.7",
